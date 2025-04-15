d0bd95969a4c13f55ea65291a6a3b8cba6c1f4e3
Avinash-Samantra <avinash02175@gmail.com>
2025-04-15 20:46:16 +0530

    second commit

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -13,14 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json()); 
 
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 	});
 }
 
--- mern/frontend/src/store/product.js
+++ mern/frontend/src/store/product.js
@@ -30,7 +30,6 @@ export const useProductStore = create((set) => ({
 		const data = await res.json();
 		if (!data.success) return { success: false, message: data.message };
 
-		// update the ui immediately, without needing a refresh
 		set((state) => ({ products: state.products.filter((product) => product._id !== pid) }));
 		return { success: true, message: data.message };
 	},
@@ -45,7 +44,7 @@ export const useProductStore = create((set) => ({
 		const data = await res.json();
 		if (!data.success) return { success: false, message: data.message };
 
-		// update the ui immediately, without needing a refresh
+
 		set((state) => ({
 			products: state.products.map((product) => (product._id === pid ? data.data : product)),
 		}));
